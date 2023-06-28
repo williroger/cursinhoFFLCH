1ef7cf6d0c4ded1e4f86f900d825d2eff877c9cc
Roger <72757812+williroger@users.noreply.github.com>
2023-06-28 10:31:24 -0300

    atualização api redacao

--- script/main.js
+++ script/main.js
@@ -1,19 +1,22 @@
-window.onload = function() {
+function resultado() {
+	console.log("resultado")
     // Carrega o arquivo JSON
     fetch('https://williroger.github.io/cursinhoFFLCH/data/red.json')
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
+			
             // Manipula os dados JSON
-			var zero = JSON.stringify(data['10']['zero']);
-			var motivoZero = JSON.stringify(data['10']['motivo_zero']);
-			var c1 = parseInt(JSON.stringify(data['10']['c1']));
-			var c2 = parseInt(JSON.stringify(data['10']['c2']));
-			var c3 = parseInt(JSON.stringify(data['10']['c3']));
-			var c4 = parseInt(JSON.stringify(data['10']['c4']));
-			var c5 = parseInt(JSON.stringify(data['10']['c5']));
-			var comentario = JSON.stringify(data['10']['comentario']);
+			var idEstudante = inputIdEstudante
+			var zero = JSON.stringify(data[`${idEstudante}`]['zero']);
+			var motivoZero = JSON.stringify(data[`${idEstudante}`]['motivo_zero']);
+			var c1 = parseInt(JSON.stringify(data[`${idEstudante}`]['c1']));
+			var c2 = parseInt(JSON.stringify(data[`${idEstudante}`]['c2']));
+			var c3 = parseInt(JSON.stringify(data[`${idEstudante}`]['c3']));
+			var c4 = parseInt(JSON.stringify(data[`${idEstudante}`]['c4']));
+			var c5 = parseInt(JSON.stringify(data[`${idEstudante}`]['c5']));
+			var comentario = JSON.stringify(data[`${idEstudante}`]['comentario']);
 
 			let somRedacao = c1+c2+c3+c4+c5
 			
@@ -21,9 +24,32 @@ window.onload = function() {
             dataContainer.innerHTML = 'TOTAL: '+somRedacao;
 			
 			var dataContainer = document.getElementById('comentario');
-            dataContainer.innerHTML = comentario;
+            dataContainer.innerHTML = comentario;				
+			
+			var dataContainer = document.getElementById('c1');
+            dataContainer.innerHTML = c1;
+			
+			var dataContainer = document.getElementById('c2');
+            dataContainer.innerHTML = c2;
+			
+			var dataContainer = document.getElementById('c3');
+            dataContainer.innerHTML = c3;
+			
+			var dataContainer = document.getElementById('c4');
+            dataContainer.innerHTML = c4;
+			
+			var dataContainer = document.getElementById('c5');
+            dataContainer.innerHTML = c5;
+			
+		
         })
         .catch(function(error) {
             console.log('Ocorreu um erro ao carregar o arquivo JSON:', error);
         });
 };
+
+
+function SendInput(){
+	var inputIdEstudante = document.getElementById("inputIdEstudante").value
+	Resultado()
+}
