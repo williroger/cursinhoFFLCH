d768f23ce1074bf1858c347a454f96d85e918b3c
Roger <72757812+williroger@users.noreply.github.com>
2023-06-27 21:35:56 -0300

    redacao-api

---
+++ script/main.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    // Carrega o arquivo JSON
+    fetch('data.json')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            // Manipula os dados JSON
+            var dataContainer = document.getElementById('data-container');
+            dataContainer.innerHTML = JSON.stringify(data);
+        })
+        .catch(function(error) {
+            console.log('Ocorreu um erro ao carregar o arquivo JSON:', error);
+        });
+};
